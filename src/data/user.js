536a635f9e8a1a58034c4f75f20088ea9665188f
Sparkles2000 <rucker.anna2000@gmail.com>
2021-05-18 01:40:05 -0500

    modified user.js

--- src/data/user.js
+++ src/data/user.js
@@ -1,6 +1,12 @@
-const username = "Liza";
-const city = "New York";
+import react from "react";
+import About from "../components/About";
+import Home from "../components/Home";
+import App from "../components/App";
+export const { unstable_renderSubtreeIntoContainer } = require("react-dom");
 
-const image = "https://i.imgur.com/mV8PQxj.gif";
+export const username = "Liza";
+export const city = "New York";
+
+export const image = "https://i.imgur.com/mV8PQxj.gif";
 
 // export all three variables as named exports
